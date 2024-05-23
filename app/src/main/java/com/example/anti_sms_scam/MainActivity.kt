3a23ca35c38999d04ae1bbfb2650011aee66d9e1
philscheuerer <philipp.scheuerer@st.oth-regensburg.de>
2024-05-23 23:53:25 +0200

    initial commit

---
+++ app/src/main/java/com/example/anti_sms_scam/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.anti_sms_scam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
