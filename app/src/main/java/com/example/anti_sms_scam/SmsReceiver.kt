fd83efc376d9656b592e9d67c9472ff89a4d9fd4
WinstonTNguyen <52378568+WinstonTNguyen@users.noreply.github.com>
2024-05-24 22:50:30 +0200

    added notification 4 flagged sms

--- app/src/main/java/com/example/anti_sms_scam/SmsReceiver.kt
+++ app/src/main/java/com/example/anti_sms_scam/SmsReceiver.kt
@@ -1,13 +1,10 @@
 package com.example.anti_sms_scam
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
-import android.os.Bundle
+import android.graphics.BitmapFactory
 import android.telephony.SmsMessage
 import android.util.Log
 import androidx.core.app.NotificationCompat
@@ -23,7 +20,8 @@ class SmsReceiver : BroadcastReceiver() {
         val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
         val builder = NotificationCompat.Builder(context, "SMS_RECEIVED_CHANNEL")
-            .setSmallIcon(R.drawable.ic_sms_notification)
+            .setSmallIcon(R.drawable.ic_sms_notification_small)
+            .setLargeIcon(BitmapFactory.decodeResource(context.resources, R.drawable.ic_sms_notification_large))
             .setContentTitle("Scam Message Detected.")
             .setContentText("Click here for more information.")
             .setStyle(NotificationCompat.BigTextStyle().bigText(message))
